80e411d44fde0806f017bc2f4570ac5d3af59979
YaPhut <namogw@gmail.com>
2026-06-29 11:46:59 -0700

    Fixed Network bugs and create OR_Network

--- pattern_match_O_n.py
+++ pattern_match_O_n.py
@@ -1,6 +1,18 @@
 from snn.network import Network
 
-class PatternMatcher(Network):
+class OR_Network(Network):
     def __init__(self):
         super().__init__(2, 1)
-        self.add_neuron(("OR", 0, 0))
+        self.add_neuron(("OR", 1, 0))
+        self.add_synapse(("Input 0", "OR", 1))
+        self.add_synapse(("Input 1", "OR", 1))
+        self.add_synapse(("OR", "Output 0", 1))
+        self.setup_network()
+
+matcher = OR_Network()
+print(matcher.step())
+print(matcher.step([0, 1]))
+print(matcher.step())
+print(matcher.step([1, 0]))
+print(matcher.step())
+print(matcher.step())
--- snn/network.py
+++ snn/network.py
@@ -4,7 +4,7 @@ class Network:
     def __init__(self, num_inputs, num_outputs):
         self.num_inputs = num_inputs
         self.num_outputs = num_outputs
-        self.neurons = [(0, 0) for i in range(num_inputs + num_outputs)]
+        self.neurons = [(1, 0) for _ in range(num_inputs)] + [(1, 0) for _ in range(num_outputs)]
         self.names = [f"Input {i}" for i in range(num_inputs)] + [f"Output {i}" for i in range(num_outputs)]
         self.synapses = []
         self.threshold = None
@@ -19,33 +19,36 @@ class Network:
         self.neurons.append(neuron[1:])
 
     def add_synapse(self, synapse):
+        synapse = list(synapse)
         for i in range(2):
             if isinstance(synapse[i], str):
                 if synapse[i] not in self.names:
                     raise ValueError("Neuron name '" + synapse[i] + "' not found.")
                 synapse[i] = self.names.index(synapse[i])
         # (presynaptic, postsynaptic, weight)
-        self.synapses.append(synapse)
+        self.synapses.append(tuple(synapse))
 
     def setup_network(self):
         n = len(self.neurons)
         threshold, decay = zip(*self.neurons)
-        self.threshold = np.array(threshold)
-        self.decay = np.array(decay)
-        self.voltage = np.zeros_like(threshold)
+        self.threshold = np.array(threshold).astype(np.float64)
+        self.decay = np.array(decay).astype(np.float64)
+        self.voltage = np.zeros_like(threshold).astype(np.float64)
 
         self.synapse_mat = np.zeros((n, n))
         for synapse in self.synapses:
             self.synapse_mat[synapse[0], synapse[1]] += synapse[2]
-        self.x = np.zeros((1, n))
+        self.synapse_mat = self.synapse_mat.astype(np.float64)
+        self.x = np.zeros((1, n)).astype(bool)
 
-    def step(self, inputs):
-        self.x[-1][:self.num_inputs] = inputs
+    def step(self, inputs=None):
+        if inputs is not None:
+            self.x[-1][:self.num_inputs] = np.array(inputs)
 
         self.voltage *= self.decay
         self.voltage += self.x[-1] @ self.synapse_mat
         fires = (self.voltage >= self.threshold)
         self.voltage[fires] = 0
-        self.x = np.concatenate([self.x, fires], axis=0)
+        self.x = np.concatenate([self.x, fires[np.newaxis, :]], axis=0)
 
         return fires[self.num_inputs:self.num_inputs + self.num_outputs]
